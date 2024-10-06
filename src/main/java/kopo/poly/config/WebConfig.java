3fe4744dd93893b23f315b1126c9c9ff993df39a
masche14 <zskfnxh134@gmail.com>
2024-10-06 16:07:24 +0900

    edit

--- src/main/java/kopo/poly/config/WebConfig.java
+++ src/main/java/kopo/poly/config/WebConfig.java
@@ -1,20 +1,26 @@
 package kopo.poly.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    @Value("${rootPath}")
+    private String rootPath;
+    @Value("${inputImgDir}")
+    private String inputImgDir;
+    @Value("${generatedImgDir}")
+    private String generatedImgDir;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/inputImg/**")
-                .addResourceLocations("file:C:/KPaaS/src/main/resources/static/inputImg/");
+                .addResourceLocations("file:"+rootPath+"/"+inputImgDir+"/");
         // C:/KPaaS/src/main/resources/static/inputImg
 
         registry.addResourceHandler("/generatedImages/**")
-                        .addResourceLocations("file:C:/KPaaS/src/main/resources/static/generatedImages/");
+                        .addResourceLocations("file:"+rootPath+"/"+generatedImgDir+"/");
 
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:C:/uploads/");
