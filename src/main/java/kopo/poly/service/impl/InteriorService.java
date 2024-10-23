9e88cd4d6fb6d4345ab543aca4ec04fbbb3d5416
masche14 <zskfnxh134@gmail.com>
2024-10-23 11:12:31 +0900

    intelij

--- src/main/java/kopo/poly/service/impl/InteriorService.java
+++ src/main/java/kopo/poly/service/impl/InteriorService.java
@@ -102,21 +102,7 @@ public class InteriorService implements IInteriorService {
     public String generateImg(String imageUrl, String prompt, String userId) throws Exception {
         log.info("이미지 생성 시작");
 
-//        if (!savedFile.exists()) {
-//            log.error("저장된 파일을 찾을 수 없습니다.");
-//            throw new IllegalArgumentException("업로드된 파일이 없습니다.");
-//        }
-//
-//        // 파일 경로
-//        String imagePath = savedFile.getAbsolutePath();
-//        String fileName = "ai_"+fileNameEncode(userId)+".png";
-//        String outputPath = "C:/uploads/"+fileName;
-
         try {
-//            // 이미지 파일을 Base64로 인코딩
-//            byte[] imageBytes = Files.readAllBytes(Paths.get(imagePath));
-//            String encodedImage = Base64.getEncoder().encodeToString(imageBytes);
-
             // API URL
             String apiUrl = "https://modelslab.com/api/v6/realtime/img2img";
 
@@ -190,88 +176,6 @@ public class InteriorService implements IInteriorService {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
-        }
-    }
-
-//    @Override
-//    public String downloadBase64Image(String base64DataUrl, String outputPath) {
-//        int retryCount = 3; // 최대 재시도 횟수
-//        int delay = 5000;   // 5초 지연 (밀리초 단위)
-//
-//        for (int i = 0; i < retryCount; i++) {
-//            try {
-//                // base64 인코딩된 데이터를 담고 있는 URL에서 데이터 가져오기
-//                URL url = new URL(base64DataUrl);
-//                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//                conn.setRequestMethod("GET");
-//                log.info("인코딩 페이지 로드");
-//
-//                int responseCode = conn.getResponseCode();
-//                if (responseCode == HttpURLConnection.HTTP_OK) {
-//                    // 링크로부터 데이터를 읽어오기
-//                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
-//                    StringBuilder base64Data = new StringBuilder();
-//                    String line;
-//                    while ((line = br.readLine()) != null) {
-//                        base64Data.append(line.trim());
-//                    }
-//                    log.info("데이터 읽어오기");
-//
-//                    log.info("이미지 변환 시작");
-//                    // base64 문자열을 디코딩하여 이미지로 변환
-//                    byte[] imageBytes = Base64.getDecoder().decode(base64Data.toString());
-//                    log.info("이미지 변환 완료");
-//
-//                    // 파일로 저장
-//                    try (FileOutputStream fos = new FileOutputStream(outputPath)) {
-//                        fos.write(imageBytes);
-//                        log.info("이미지 파일이 '" + outputPath + "'로 저장되었습니다.");
-//                    }
-//
-//                    return outputPath.split(":")[1];
-//                } else {
-//                    log.error("HTTP 요청 실패: 응답 코드 " + responseCode);
-//                }
-//
-//            } catch (Exception e) {
-//                log.error("오류 발생: ", e);
-//            }
-//
-//            // 재시도 전에 일정 시간 대기
-//            try {
-//                log.info("재시도 대기 중: " + (i + 1) + "번째 시도");
-//                Thread.sleep(delay); // 5초 대기
-//            } catch (InterruptedException ie) {
-//                Thread.currentThread().interrupt(); // 현재 스레드의 인터럽트를 복구
-//                break;
-//            }
-//        }
-//
-//        return null;
-//    }
-
-    public void delTempFolder() throws Exception {
-        String folderPath = "C:/uploads/";
-        File folder = new File(folderPath);
-
-        // 폴더가 존재하고 디렉토리인지 확인
-        if (folder.exists() && folder.isDirectory()) {
-            File[] files = folder.listFiles(); // 폴더 내 파일 목록 가져오기
-
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isFile()) {
-                        // 파일 삭제
-                        if (file.delete()) {
-                            System.out.println(file.getName() + " 파일이 삭제되었습니다.");
-                        } else {
-                            System.out.println(file.getName() + " 파일을 삭제할 수 없습니다.");
-                        }
-                    }
-                }
-            }
-        } else {
-            System.out.println("해당 폴더가 존재하지 않거나 디렉토리가 아닙니다.");
         }
     }
 
